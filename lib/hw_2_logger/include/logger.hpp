ae8f08e74ebb1e5fd1770bb6fccc2e87f7064a83
akhramov36 <akhramov36@yandex-team.ru>
2020-10-16 21:29:39 +0300

    clang

--- lib/hw_2_logger/include/logger.hpp
+++ lib/hw_2_logger/include/logger.hpp
@@ -1,60 +1,55 @@
 #pragma once
 
-#include <string>
 #include <fstream>
+#include <string>
 
-namespace log{
+namespace log {
 
-enum class Level{
-  DEBUG,
-  INFO,
-  WARN,
-  ERROR
-};
+enum class Level { DEBUG, INFO, WARN, ERROR };
 
-std::string LevelToString(const Level& level);
+std::string LevelToString(const Level &level);
 
 class BaseLogger {
 public:
-  virtual void Log(const Level& level, const std::string& msg);
-  virtual void Debug(const std::string& msg);
-  virtual void Info(const std::string& msg);
-  virtual void Warn(const std::string& msg);
-  virtual void Error(const std::string& msg);
-  void SetLevel(const Level& level) {level_ = level;}
-  Level GetCurrentLevel(){return level_;}
-  virtual void Flush(){}
+  virtual void Log(const Level &level, const std::string &msg);
+  virtual void Debug(const std::string &msg);
+  virtual void Info(const std::string &msg);
+  virtual void Warn(const std::string &msg);
+  virtual void Error(const std::string &msg);
+  void SetLevel(const Level &level) { level_ = level; }
+  Level GetCurrentLevel() { return level_; }
+  virtual void Flush() {}
 
 private:
   Level level_ = Level::INFO;
-  virtual void PrintLog(const Level& level, const std::string& msg){}
+  virtual void PrintLog(const Level &level, const std::string &msg) {}
 
 protected:
-  virtual void PrintLogInternal(std::ostream& stream, const Level& level, const std::string& msg);
+  virtual void PrintLogInternal(std::ostream &stream, const Level &level,
+                                const std::string &msg);
 };
 
-class StdoutLogger : public BaseLogger{
+class StdoutLogger : public BaseLogger {
 private:
   void Flush() override;
-  void PrintLog(const Level& level, const std::string& msg) override;
+  void PrintLog(const Level &level, const std::string &msg) override;
 };
 
-class StderrLogger : public BaseLogger{
+class StderrLogger : public BaseLogger {
 private:
-
   void Flush() override;
-  void PrintLog(const Level& level, const std::string& msg) override;
+  void PrintLog(const Level &level, const std::string &msg) override;
 };
-
 
 class FileLogger : public BaseLogger {
-  public:
-  explicit FileLogger(const std::string& filename);
+public:
+  explicit FileLogger(const std::string &filename);
   ~FileLogger();
+
 private:
   std::ofstream log_file_;
   void Flush() override;
-  void PrintLog(const Level& level, const std::string& msg) override;
+  void PrintLog(const Level &level, const std::string &msg) override;
 };
 
-}
+} // namespace log
